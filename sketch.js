1054555c98ee855933c7394d7e3ecf7bffca5a07
DracTheDino <sidtrip06@gmail.com>
2024-02-05 16:59:45 +0530

    add next generation

--- sketch.js
+++ sketch.js
@@ -30,6 +30,45 @@ function draw() {
 			rect(i * cellsize, j * cellsize, cellsize);
 		}
 	}
+
+	nextGen(arr);
 }
 
-function nextGen(arr) {}
+function nextGen(arr) {
+	var arrN = arr;
+	for (i = 0; i < xGrid; i++) {
+		for (j = 0; j < yGrid; j++) {
+			let sum = countNeighbours(arr, i, j);
+
+			// overpopulation
+			if (arr[i][j] == 1 && sum < 2) {
+				arrN[i][j] = 0;
+			}
+			// underpopulation
+			if (arr[i][j] == 1 && sum > 3) {
+				arrN[i][j] = 0;
+			}
+			// reproduction
+			if (arr[i][j] == 0 && sum == 3) {
+				arrN[i][j] = 1;
+			}
+		}
+	}
+	arr = arrN;
+}
+
+// find sum of neighbourhood elements with wraparound for boundary elements
+function countNeighbours(arr, x, y) {
+	let sum = 0;
+
+	for (let i = -1; i < 2; i++) {
+		for (let j = -1; j < 2; j++) {
+			let row = (x + i + xGrid) % xGrid;
+			let col = (y + j + yGrid) % yGrid;
+			sum += arr[row][col];
+		}
+	}
+
+	sum -= arr[x][y];
+	return sum;
+}
